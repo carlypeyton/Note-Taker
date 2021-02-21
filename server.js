aef8f09a3d1508bfc6eb199cbbca25b9e2a08759
Carly Peyton <carlypeyton@gmail.com>
2021-02-20 19:22:49 -0500

    Create server with API and HTML routes. Require express.

---
+++ server.js
@@ -0,0 +1,27 @@
+//////////////////NODE MODULES///////////////////
+
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+
+///////////////EXPRESS CONFIGURATION///////////////
+
+//Create express server
+const app = express();
+//Set initial port
+const PORT = process.env.PORT || 3000;
+//Set express app to handle data parsing
+app.use(express.static(__dirname));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+///////////////////ROUTES/////////////////////
+
+require("./routes/apiroutes")(app);
+require("./routes/htmlroutes")(app);
+
+/////////////////////START SERVER//////////////////////
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+});
